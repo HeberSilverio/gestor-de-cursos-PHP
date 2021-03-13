0597a9c90bbe2bab742f40cb804a48979d85f980
Héber Silvério <hebersilverio@gmail.com>
2021-03-13 13:01:42 -0300

    teoria de classe e objeto

---
+++ Nova pasta/classe.php
@@ -0,0 +1,45 @@
+<div class="titulo">Conceito de classe</div>
+
+<h4> Classe tem <b>atributos e comportamentos.</b></h4>
+<img src="img/anatomia-classe.png" alt="">
+<p>
+    - Basicamente classe é uma estrutura, é um bloco de código e dentro deste bloco você vai agrupar atributos e comportamentos da mesma forma que os objetos do mundo real possuem atributos e comportamentos.<br>
+    - <b>Atributos</b> são Dados.<br>
+    - <b>Comportamentos</b> são Métodos(funções). Funções dentro da classe são chamadas de método.
+    <img src="img/anatomia-classe1.png" alt="">
+</p>
+<h4>Classe define um tipo! E o tipo é uma estrutura de dados</h4>
+<p>Uma classe define um novo tipo personalizado que você esta criando, um tipo que vai atender a necessidade do seu software, da aplicação que você esta construindo.
+<br>
+- O tipo define uma quantidade de atributos e uma quantidade de comportamentos e isso é chamada de estrutura de dados. <br>
+<img src="img/anatomia-classe2.png" alt="">
+- Por exemplo se uma classe chamada cliente cria 3 atributos (Nome, DtNasc, Telefone) todos os clientes terão estes 3 atributos e essa é a estrutura de dados. <br>
+- O que vai mudar é que cada um vai ter seus próprios valores, mas a estrutura, quais são os atributos e comportamentos tudo isso é padronizado porque quem define isso é o tipo, ou seja a estrutura de dados. <br>
+- E como a partir desta mesma estrutura eu criei vários objetos ou instâncias, significa que estes dados vão ter exatamente as mesmas características, o que vai mudar são os valores associados a estes atributos.
+
+</p>
+
+<h4>objetos</h4>
+<p>Quando você cria um dado a partir de uma classe, nós dizemos que você criou um objeto daquela classe. <br>
+Podemos uma única classe gerando vários objetos diferentes.</p>
+<img src="img/anatomia-classe3.png" alt="">
+<h4>Objeto == instância | Objeto é sinônimo de instância</h4>
+<img src="img/anatomia-classe4.png" alt="">
+<p>Exemplo: Eu tenho uma classe chamada clientes e eu criei 3 instâncias de clientes, ou seja eu criei 3 objetos de clientes, 3 itens de clientes (Jose, João e Maria). </p>
+
+<h4>A diferença entre classe e objeto</h4>
+<img src="img/anatomia-classe5.png" alt="">
+<p> - A classe é o molde, e o objeto é aquilo que é gerado a partir do molde. <br>
+<img src="img/anatomia-classe6.png" alt="">Na metáfora a classe é o molde que se usa para aplicar na massinha e gerar o objeto daquele molde. <br> Quantas classes eu preciso para gerar mil bonecos de massinhas? Um molde(classe) apenas.<br>
+- Atributos de uma classe <br>
+- Método de uma classe <br>
+- Os membros de uma classe <br>
+<img src="img/classe.PNG" alt="">
+<img src="img/anatomia-classe7.png" alt="">
+</p>
+<style>
+h4 {
+    text-transform: uppercase;
+    color: #6A5ACD;
+}
+</style>
