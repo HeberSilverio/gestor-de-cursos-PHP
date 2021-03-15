769121481b6a88f95af228c74b5acc0910ad7988
Héber Silvério <hebersilverio@gmail.com>
2021-03-15 14:20:21 -0300

    novos prints de aulas

--- classes_objetos/paradigmas.php
+++ classes_objetos/paradigmas.php
@@ -1,5 +1,35 @@
 <div class="titulo">Paradigmas</div>
-<p>Aula 141 paradigmas</p>
-<img src="img/paradigmas1.png" alt="">
-<img src="img/paradigmas2.png" alt="">
-<img src="img/paradigmas3.png" alt="">
+
+    <div class="container">
+    <h4>Aula 141 paradigmas</h4>
+        <div class="row">
+            <div class="col-12 col-md-12 col-lg-12">
+                <img src="img/paradigmas1.png" alt="">
+                <img src="img/paradigmas2.png" alt="">
+                <img src="img/paradigmas3.png" alt="">
+            </div>
+
+            <div class="col-12 col-md-12 col-lg-12">
+                <h4>Pilares da orientação a objetos</h4>
+                <p>Você precisa saber chamar os métodos. O que você precisa conhecer são: <br>
+                    - Quais são os parâmetros que esse método recebe? <br>
+                    - Que tipo de retorno ele vai devolver? string, inteiro, real, array? <br>
+                    </p>
+                <img src="img/PilaresOO.png" alt="">
+                <img src="img/PilaresOO1.png" alt="">
+                <img src="img/encapsulamento1.png" alt="">
+                <img src="img/encapsulamento2.png" alt="">
+                <img src="img/encapsulamento3.png" alt="">
+                <img src="img/encapsulamento4.png" alt="">
+            </div>
+        </div>
+    </div>
+
+
+<style>
+    h4 {
+        text-transform: uppercase;
+        color: #6A5ACD;
+        margin-top: 15px;
+    }
+</style>
